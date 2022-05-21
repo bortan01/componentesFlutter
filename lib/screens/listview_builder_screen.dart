641619baeb59e7b9645705178d0a393aada08efd
Boris Ricardo Miranda <brmiranda009@gmail.com>
2022-05-21 17:29:51 -0600

    terminando app de componentes

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,14 +1,113 @@
+import 'package:componentes/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
-   
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  bool isLoading = false;
+
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      // print('${scrollController.position.pixels} ,${scrollController.position.maxScrollExtent}');
+      if (scrollController.position.pixels + 50 >=
+          scrollController.position.maxScrollExtent) {
+        print("pidieneod mas");
+        fetchData();
+      }
+    });
+  }
+
+  void add5() {
+    print("cargando mas");
+    final int lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
+
+  Future fetchData() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    add5();
+    isLoading = false;
+    setState(() {});
+    scrollController.animateTo(scrollController.position.pixels + 120, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('ListViewBuilderScreen'),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppTheme.primary,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: Stack(
+          children: [
+            RefreshIndicator(
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: imagesIds.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInImage(
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                      height: 300,
+                      placeholder: const AssetImage('assets/gif/jar-loading.gif'),
+                      image: NetworkImage(
+                          'https://picsum.photos/500/300?image=${imagesIds[index]}'));
+                },
+              ),
+            ),
+            if (isLoading)
+              Positioned(
+                bottom: 40,
+                left: size.width * 0.5 - 30,
+                child: const LoadingIcon(),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId +1);
+    add5();
+  }
+}
+
+class LoadingIcon extends StatelessWidget {
+  const LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.5), shape: BoxShape.circle),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primary,
       ),
     );
   }
